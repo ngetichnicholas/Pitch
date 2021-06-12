f8295933a1493fd5b4829d2292a62b25ca41a59e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-12 10:31:41 +0300

    Initialize flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,22 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 
+#Create app instance
 def create_app(config_name):
   app = Flask(__name__)
   app.config.from_object(config_options[config_name])
   from .auth import auth as authentication_blueprint
   from .main import main as main_blueprint
 
-  
+  #Registering bluenprints
+  app.register_blueprint(authentication_blueprint)
+  app.register_blueprint(main_blueprint)
+
+  #Initializing flask extensions
+  login_manager.init_app(app)
+  db.init_app(app)
+  bootstrap.init_app(app)
+  configure_uploads(app,photos)
+  mail.init_app(app)
+
+  return app
