40110626e44d898b49c56360dd5bdcb28a6c85f8
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-12 10:27:26 +0300

    Create an instance of app

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,11 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+
+def create_app(config_name):
+  app = Flask(__name__)
+  app.config.from_object(config_options[config_name])
+  from .auth import auth as authentication_blueprint
+  from .main import main as main_blueprint
+
+  
