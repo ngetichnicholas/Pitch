1a6e4a39ab198ea5aa8340f5b0bcca4f8b289053
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-13 08:09:30 +0300

    Add comment class to model

--- app/models.py
+++ app/models.py
@@ -55,6 +55,25 @@ class Pitch(db.Model):
     db.session.add(self)
     db.session.commit()
 
-      
   def __repr__(self):
     return f'Pitch {self.post}'
+
+class Comment(db.Model):
+  __tablename__ = 'comments'
+  id = db.Column(db.Integer, primary_key=True)
+  comment = db.Column(db.Text(),nullable = False)
+  user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+  pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+
+  def save_c(self):
+    db.session.add(self)
+    db.session.commit()
+
+  @classmethod
+  def get_comments(cls,pitch_id):
+    comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+    return comments
+  
+  def __repr__(self):
+    return f'comment:{self.comment}'
