e15db15ef5cf9574acf542643133f6f7755f12ea
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-14 13:14:53 +0300

    Update Email configuration

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
 
   #Email configurations
   MAIL_SERVER = 'smtp.gmail.com'
-  MAIL_PORT = 465
-  MAIL_USE_TLS = False
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
   MAIL_USE_SSL = True
   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
