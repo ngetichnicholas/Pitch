3d5eb0618168951f1bcb78ce7c993a413319238f
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-14 13:19:38 +0300

    Remove SSL

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ class Config:
   MAIL_SERVER = 'smtp.gmail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
-  MAIL_USE_SSL = True
   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
   #PHOTOS UPLOAD CONFIGURATION
